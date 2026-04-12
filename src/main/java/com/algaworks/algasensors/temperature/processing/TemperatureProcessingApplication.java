f4ff102b75f099c2428c92668788abc07d401c99
Marcos Oliveira <ziguifrido@users.noreply.github.com>
2026-04-11 23:24:22 -0400

    Initializing projects

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureProcessingApplication.class, args);
+	}
+
+}
